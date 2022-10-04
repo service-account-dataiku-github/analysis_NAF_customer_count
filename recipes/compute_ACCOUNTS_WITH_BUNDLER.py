37d34a52dfb78e06cf8d41f666bbd7eb40fca7af
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-10-04 20:50:03 +0000

    Saved recipe: NAFCUSTOMER.compute_ACCOUNTS_WITH_BUNDLER

--- recipes/compute_ACCOUNTS_WITH_BUNDLER.py
+++ recipes/compute_ACCOUNTS_WITH_BUNDLER.py
@@ -13,14 +13,8 @@ ACCOUNT_BUNDLER_LIST_df = ACCOUNT_BUNDLER_LIST.get_dataframe()
 ACCOUNT_BUNDLER_LIST_df['IS_BUNDLER'] = True
 ACCOUNT_BUNDLER_LIST_df = ACCOUNT_BUNDLER_LIST_df[['EDW_CUSTOMER_NAME','IS_BUNDLER']]
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 df = pd.merge(NAFCUSTOMER_C360_ACCOUNTS_df,ACCOUNT_BUNDLER_LIST_df, how='left', on='EDW_CUSTOMER_NAME')
 df['DUNS'] = df['DUNS'].astype('Int64', errors='ignore')
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
 ACCOUNTS_WITH_BUNDLER_df = df
 
