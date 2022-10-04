b806b01f54e68861b4d4b617148dbca8ec1b4134
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-10-04 21:27:46 +0000

    Recipe NAFCUSTOMER.compute_ACCOUNTS_WITH_BUNDLER saved through notebook notebook editor for compute_ACCOUNTS_WITH_BUNDLER

--- recipes/compute_ACCOUNTS_WITH_BUNDLER.py
+++ recipes/compute_ACCOUNTS_WITH_BUNDLER.py
@@ -16,9 +16,17 @@ ACCOUNT_BUNDLER_LIST_df = ACCOUNT_BUNDLER_LIST_df[['EDW_CUSTOMER_NAME','IS_BUNDL
 df = pd.merge(NAFCUSTOMER_C360_ACCOUNTS_df,ACCOUNT_BUNDLER_LIST_df, how='left', on='EDW_CUSTOMER_NAME')
 df['DUNS'] = df['DUNS'].astype('Int64', errors='ignore')
 
-df.loc[df["IS_BUNDLER"]] = np.nan
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.loc[df["IS_BUNDLER"].isnull(),'IS_BUNDLER'] = False
+df.loc[df["IS_BUNDLER"],'EDW_CUSTOMER_NAME'] = np.nan
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 ACCOUNTS_WITH_BUNDLER_df = df
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 ACCOUNTS_WITH_BUNDLER = dataiku.Dataset("ACCOUNTS_WITH_BUNDLER")
 ACCOUNTS_WITH_BUNDLER.write_with_schema(ACCOUNTS_WITH_BUNDLER_df)
