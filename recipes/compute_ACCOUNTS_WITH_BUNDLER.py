0904a9db317f7651c157a3dacf48a10e1b130a1e
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-10-04 20:47:05 +0000

    Recipe NAFCUSTOMER.compute_ACCOUNTS_WITH_BUNDLER saved through notebook notebook editor for compute_ACCOUNTS_WITH_BUNDLER

--- recipes/compute_ACCOUNTS_WITH_BUNDLER.py
+++ recipes/compute_ACCOUNTS_WITH_BUNDLER.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -9,13 +10,31 @@ NAFCUSTOMER_C360_ACCOUNTS_df = NAFCUSTOMER_C360_ACCOUNTS.get_dataframe()
 ACCOUNT_BUNDLER_LIST = dataiku.Dataset("ACCOUNT_BUNDLER_LIST")
 ACCOUNT_BUNDLER_LIST_df = ACCOUNT_BUNDLER_LIST.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+ACCOUNT_BUNDLER_LIST_df['IS_BUNDLER'] = True
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+ACCOUNT_BUNDLER_LIST_df = ACCOUNT_BUNDLER_LIST_df[['EDW_CUSTOMER_NAME','IS_BUNDLER']]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(len(NAFCUSTOMER_C360_ACCOUNTS_df))
+print(len(ACCOUNT_BUNDLER_LIST_df))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+ACCOUNT_BUNDLER_LIST_df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(len(NAFCUSTOMER_C360_ACCOUNTS_df))
+df = pd.merge(NAFCUSTOMER_C360_ACCOUNTS_df,ACCOUNT_BUNDLER_LIST_df, how='left', on='EDW_CUSTOMER_NAME')
+print(len(df))
+df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-ACCOUNTS_WITH_BUNDLER_df = ... # Compute a Pandas dataframe to write into ACCOUNTS_WITH_BUNDLER
-
+ACCOUNTS_WITH_BUNDLER_df = df
 
 # Write recipe outputs
 ACCOUNTS_WITH_BUNDLER = dataiku.Dataset("ACCOUNTS_WITH_BUNDLER")
