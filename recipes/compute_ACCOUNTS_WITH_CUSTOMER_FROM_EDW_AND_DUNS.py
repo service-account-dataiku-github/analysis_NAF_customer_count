b6b43a15078fea4096c6aea292389bc2a2411e84
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-10-07 14:56:28 +0000

    Recipe NAFCUSTOMER.compute_ACCOUNTS_WITH_CUSTOMER_FROM_EDW_AND_DUNS saved through notebook notebook editor for compute_ACCOUNTS_WITH_CUSTOMER_FROM_EDW_AND_DUNS

--- recipes/compute_ACCOUNTS_WITH_CUSTOMER_FROM_EDW_AND_DUNS.py
+++ recipes/compute_ACCOUNTS_WITH_CUSTOMER_FROM_EDW_AND_DUNS.py
@@ -8,12 +8,10 @@ import string
 # Read recipe inputs
 ACCOUNTS_WITH_BUNDLER_AND_DUNS = dataiku.Dataset("ACCOUNTS_WITH_BUNDLER_AND_DUNS")
 ACCOUNTS_WITH_BUNDLER_AND_DUNS_df = ACCOUNTS_WITH_BUNDLER_AND_DUNS.get_dataframe()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 df = ACCOUNTS_WITH_BUNDLER_AND_DUNS_df
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df.head()
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 import warnings
 warnings.filterwarnings(action='once')
 
@@ -54,11 +52,26 @@ df.loc[df["EDW_CUSTOMER_NAME"].isnull(),'EDW_STATE'] = "None"
 df.loc[~df["EDW_CUSTOMER_NAME"].isnull(),'EDW_STATE'] = "Set"
 
 df['CUSTOMER'] = np.nan
+df['CUST_CALC_SOURCE'] = 'Unknown'
 df.loc[~df["EDW_CUSTOMER_NAME"].isnull(),'CUSTOMER'] = df["EDW_CUSTOMER_NAME"]
+df.loc[~df["EDW_CUSTOMER_NAME"].isnull(),'CUST_CALC_SOURCE'] = "EDW"
+
+df.loc[(df["CUSTOMER"].isnull())&(~df["DNB_CUSTOMER_NAME"].isnull()),'CUST_CALC_SOURCE'] = "DNB"
 df.loc[df["CUSTOMER"].isnull(),'CUSTOMER'] = df["DNB_CUSTOMER_NAME"]
+
+df.loc[df["CUSTOMER"].isnull(),'CUST_CALC_SOURCE'] = 'ACCOUNT'
 df.loc[df["CUSTOMER"].isnull(),'CUSTOMER'] = df["CUSTOMER_ACCOUNT_NAME"]
 
-df.CUSTOMER.value_counts(dropna=False)
+# RULE SETs
+# Rule 001
+df.loc[(df['CUSTOMER']=='QUANTA SERVICES INC')|(df['CUSTOMER']=='QUANTA SERVICES'),"CUSTOMER"] = "QUANTA SERVICES INC"
+df.loc[(df['CUSTOMER']=='QUANTA SERVICES INC')|(df['CUSTOMER']=='QUANTA SERVICES'),"CUST_CALC_SOURCE"] = "RULE 001"
+
+# Rule 002
+df.loc[(df['CUSTOMER']=='0113 WINDSTREAM COMM')|(df['CUSTOMER']=='0113 WINDSTREAM COMM (2)'),"CUSTOMER"] = "0113 WINDSTREAM COMM"
+df.loc[(df['CUSTOMER']=='0113 WINDSTREAM COMM')|(df['CUSTOMER']=='0113 WINDSTREAM COMM (2)'),"CUST_CALC_SOURCE"] = "RULE 002"
+
+df.CUST_CALC_SOURCE.value_counts()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 ACCOUNTS_WITH_CUSTOMER_FROM_EDW_AND_DUNS_df = df
