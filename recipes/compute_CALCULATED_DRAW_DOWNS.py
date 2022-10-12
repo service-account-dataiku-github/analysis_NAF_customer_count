60e62f594b05ba34c2e66f9959dfac561029bce3
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-10-12 17:53:49 +0000

    Recipe NAFCUSTOMER.compute_CALCULATED_DRAW_DOWNS saved through notebook notebook editor for compute_CALCULATED_DRAW_DOWNS

--- recipes/compute_CALCULATED_DRAW_DOWNS.py
+++ recipes/compute_CALCULATED_DRAW_DOWNS.py
@@ -4,6 +4,53 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+import pickle
+from dateutil.relativedelta import relativedelta
+import gc
+from re import finditer
+
+## Find DD DU
+from helper import preprocess_data
+from patterns import find_drawdowns, find_drawups
+
+## MATCHING
+import name_matching
+from name_matching import name_match
+import transaction_matching
+from transaction_matching import transaction_match
+
+## CONSOLIDATION
+from consolidation import combine_matches, consolidate_matches, find_attritions, find_new_accounts, get_attrition_status, get_new_account_status
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+start_date = dataiku.get_custom_variables()['start_date']
+end_date = dataiku.get_custom_variables()['end_date']
+
+consistency = int(dataiku.get_custom_variables()['consistency'])
+drawdown_period_average = int(dataiku.get_custom_variables()['drawdown_period_average'])
+drawdown = int(dataiku.get_custom_variables()['drawdown'])
+drawdown_fwd_check = int(dataiku.get_custom_variables()['drawdown_fwd_check'])
+drawdown_lookback_period = int(dataiku.get_custom_variables()['drawdown_lookback_period'])
+drawup_lookfwd_period = int(dataiku.get_custom_variables()['drawup_lookfwd_period'])
+statistics_period = int(dataiku.get_custom_variables()['statistics_period'])
+inactive_period = int(dataiku.get_custom_variables()['inactive_period'])
+
+## MATCHING VARIABLES
+month_diff_h = int(dataiku.get_custom_variables()['month_diff_h'])
+month_diff_l = int(dataiku.get_custom_variables()['month_diff_l'])
+sd_mul = int(dataiku.get_custom_variables()['sd_mul'])
+max_city_distance = int(dataiku.get_custom_variables()['max_city_distance'])
+threshold_score_step1 = int(dataiku.get_custom_variables()['threshold_score_step1'])
+threshold_score_step2 = int(dataiku.get_custom_variables()['threshold_score_step2'])
+
+## RUN TYPE
+run = dataiku.get_custom_variables()['run_type']
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+def date_tz_naive(pd_s):
+    return pd.to_datetime(pd_s).apply(lambda x:x.tz_localize(None))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 NAFCUSTOMER_C360_ACCOUNTS = dataiku.Dataset("NAFCUSTOMER_C360_ACCOUNTS")
 NAFCUSTOMER_C360_ACCOUNTS_df = NAFCUSTOMER_C360_ACCOUNTS.get_dataframe()
@@ -14,7 +61,16 @@ NAFCUSTOMER_REVENUE_BY_CUSTOMER_LIMITED_df = NAFCUSTOMER_REVENUE_BY_CUSTOMER_LIM
 print(len(NAFCUSTOMER_REVENUE_BY_CUSTOMER_LIMITED_df))
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-NAFCUSTOMER_REVENUE_BY_CUSTOMER_LIMITED_df.head()
+df_v = NAFCUSTOMER_REVENUE_BY_CUSTOMER_LIMITED_df
+
+print(len(df_v))
+df_v['REVENUE_DATE'] = df_v.REVENUE_MONTH.astype(str) + "/01/" + df_v.REVENUE_YEAR.astype(str)
+df_v['REVENUE_DATE'] = date_tz_naive(df_v['REVENUE_DATE'])
+print(len(df_v))
+df_v.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df_v.columns.tolist()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
