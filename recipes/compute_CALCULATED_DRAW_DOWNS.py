a1c38761b9e9a35f14614722f19a0053eb5b1bdc
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-10-12 17:05:40 +0000

    Recipe NAFCUSTOMER.compute_CALCULATED_DRAW_DOWNS saved through notebook notebook editor for compute_CALCULATED_DRAW_DOWNS

--- recipes/compute_CALCULATED_DRAW_DOWNS.py
+++ recipes/compute_CALCULATED_DRAW_DOWNS.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -6,11 +7,18 @@ from dataiku import pandasutils as pdu
 # Read recipe inputs
 NAFCUSTOMER_C360_ACCOUNTS = dataiku.Dataset("NAFCUSTOMER_C360_ACCOUNTS")
 NAFCUSTOMER_C360_ACCOUNTS_df = NAFCUSTOMER_C360_ACCOUNTS.get_dataframe()
+print(len(NAFCUSTOMER_C360_ACCOUNTS_df))
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+NAFCUSTOMER_C360_ACCOUNTS_df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+
 
 CALCULATED_DRAW_DOWNS_df = NAFCUSTOMER_C360_ACCOUNTS_df # For this sample code, simply copy input to output
 
