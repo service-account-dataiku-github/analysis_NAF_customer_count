7c5cef5c51444a478781cebb14e03f23790144ea
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-10-15 19:48:03 +0000

    Saved recipe: NAFCUSTOMER.compute_CALCULATED_CARD_DRAW_DOWNS_FULL

---
+++ recipes/compute_CALCULATED_CARD_DRAW_DOWNS_FULL.py
@@ -0,0 +1,68 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+import pickle
+from dateutil.relativedelta import relativedelta
+import gc
+from re import finditer
+
+## Find DD DU
+from helper import preprocess_data
+from patterns import find_drawdowns, find_drawups
+
+## MATCHING
+import name_matching
+from name_matching import name_match
+import transaction_matching
+from transaction_matching import transaction_match
+
+## CONSOLIDATION
+from consolidation import combine_matches, consolidate_matches, find_attritions, find_new_accounts, get_attrition_status, get_new_account_status
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+start_date = dataiku.get_custom_variables()['start_date']
+end_date = dataiku.get_custom_variables()['end_date']
+
+consistency = int(dataiku.get_custom_variables()['consistency'])
+drawdown_period_average = int(dataiku.get_custom_variables()['drawdown_period_average'])
+drawdown = int(dataiku.get_custom_variables()['drawdown'])
+drawdown_fwd_check = int(dataiku.get_custom_variables()['drawdown_fwd_check'])
+drawdown_lookback_period = int(dataiku.get_custom_variables()['drawdown_lookback_period'])
+drawup_lookfwd_period = int(dataiku.get_custom_variables()['drawup_lookfwd_period'])
+statistics_period = int(dataiku.get_custom_variables()['statistics_period'])
+inactive_period = int(dataiku.get_custom_variables()['inactive_period'])
+
+## MATCHING VARIABLES
+month_diff_h = int(dataiku.get_custom_variables()['month_diff_h'])
+month_diff_l = int(dataiku.get_custom_variables()['month_diff_l'])
+sd_mul = int(dataiku.get_custom_variables()['sd_mul'])
+max_city_distance = int(dataiku.get_custom_variables()['max_city_distance'])
+threshold_score_step1 = int(dataiku.get_custom_variables()['threshold_score_step1'])
+threshold_score_step2 = int(dataiku.get_custom_variables()['threshold_score_step2'])
+
+## RUN TYPE
+run = dataiku.get_custom_variables()['run_type']
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+def date_tz_naive(pd_s):
+    return pd.to_datetime(pd_s).apply(lambda x:x.tz_localize(None))
+
+
+# Read recipe inputs
+NAFCUSTOMER_ACTIVE_CARDS_FULL = dataiku.Dataset("NAFCUSTOMER_ACTIVE_CARDS_FULL")
+NAFCUSTOMER_ACTIVE_CARDS_FULL_df = NAFCUSTOMER_ACTIVE_CARDS_FULL.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+CALCULATED_CARD_DRAW_DOWNS_FULL_df = NAFCUSTOMER_ACTIVE_CARDS_FULL_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+CALCULATED_CARD_DRAW_DOWNS_FULL = dataiku.Dataset("CALCULATED_CARD_DRAW_DOWNS_FULL")
+CALCULATED_CARD_DRAW_DOWNS_FULL.write_with_schema(CALCULATED_CARD_DRAW_DOWNS_FULL_df)
