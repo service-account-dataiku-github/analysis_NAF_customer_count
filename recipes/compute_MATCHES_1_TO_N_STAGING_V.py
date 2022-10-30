92be93af628174e0a78a62c907023ae4a29c1a17
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-10-30 22:50:35 +0000

    Recipe NAFCUSTOMER.compute_MATCHES_1_TO_N_STAGING_V saved through notebook notebook editor for compute_MATCHES_1_TO_N_STAGING_V

--- recipes/compute_MATCHES_1_TO_N_STAGING_V.py
+++ recipes/compute_MATCHES_1_TO_N_STAGING_V.py
@@ -11,21 +11,83 @@ CALCULATED_DRAW_DOWNS_df = CALCULATED_DRAW_DOWNS.get_dataframe()
 CALCULATED_DRAW_UPS = dataiku.Dataset("CALCULATED_DRAW_UPS")
 CALCULATED_DRAW_UPS_df = CALCULATED_DRAW_UPS.get_dataframe()
 
-print(len(CALCULATED_DRAW_DOWNS_df), "draw downs")
-print(len(CALCULATED_DRAW_UPS_df), "draw ups")
+COMMON_WORDS = dataiku.Dataset("NAFCUSTOMER_COMMON_WORDS_IN_NAMES")
+COMMON_WORDS_df = COMMON_WORDS.get_dataframe()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-#df_down_full = CALCULATED_CARD_DRAW_DOWNS_FULL_df
-#df_up_full = CALCULATED_CARD_DRAW_UPS_FULL_df
-#df_common = COMMON_WORDS_df
+df_down_full = CALCULATED_DRAW_DOWNS_df
+df_up_full = CALCULATED_DRAW_UPS_df
+df_common = COMMON_WORDS_df
 
-#df_down_full.sort_values(['CUSTOMER'], inplace=True)
-#df_up_full.sort_values(['CUSTOMER'], inplace=True)
-#df_common.sort_values(['WORD'], inplace=True)
+df_down_full.sort_values(['CUSTOMER'], inplace=True)
+df_up_full.sort_values(['CUSTOMER'], inplace=True)
+df_common.sort_values(['WORD'], inplace=True)
 
-#print(len(df_down_full), "draw downs full")
-#print(len(df_up_full), "draw ups full")
-#print(len(df_common), "common words")
+print(len(df_down_full), "draw downs full")
+print(len(df_up_full), "draw ups full")
+print(len(df_common), "common words")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import string
+
+_common_words = df_common.WORD.unique()
+print(len(_common_words), "screening against common words")
+
+class Draw_Down_Customer:
+
+    def __init__(self, name, draw_down_date, mean_dd, std_dd, active_card_max):
+
+        self.CUSTOMER = name
+        self.DRAW_DOWN_DATE = draw_down_date
+        self.ACTIVE_CARD_MAX = active_card_max
+
+        self.MATCHING_CUSTOMERS = []
+        self.PERCENT_DIFFERENCE = []
+        self.DAYS_DIFFERENCE = []
+        self.DRAW_UP_DATE = []
+
+        # remove punctuation
+        c_str = name.translate(str.maketrans('', '', string.punctuation))
+
+        f = c_str.split()
+        self.WORD_LIST = []
+        for w in f:
+            if w not in _common_words:
+                self.WORD_LIST.append(w)
+
+    def Match_Draw_Up_Customer(self, name, draw_up_date, mean_du, std_du, active_card_max):
+
+        if (self.CUSTOMER == name):
+            # exact match, already captured
+            return
+
+        c_str = name.translate(str.maketrans('', '', string.punctuation))
+
+        f = c_str.split()
+
+        check_list = []
+        for w in f:
+            if (w not in _common_words) and (len(w)>1) and (not w.isnumeric()):
+                check_list.append(w)
+
+        percent_diff = round((abs(self.ACTIVE_CARD_MAX - active_card_max) / ((self.ACTIVE_CARD_MAX+active_card_max)/2)),2)
+
+        #date_format = "%Y-%m-%d"
+        #d1_date = datetime.strptime(draw_up_date.astype(str), date_format)
+        #d2_date = datetime.strptime(self.DRAW_DOWN_DATE.astype(str), date_format)
+
+        delta_between_drop_and_rise = round(abs((draw_up_date-self.DRAW_DOWN_DATE).days)/30.,0)
+
+        for w_to_check in check_list:
+            for w in self.WORD_LIST:
+                if w_to_check==w:
+
+                    if not name in(self.MATCHING_CUSTOMERS) and(delta_between_drop_and_rise<=4)and(percent_diff<=0.5) :
+                        self.MATCHING_CUSTOMERS.append(name)
+                        self.PERCENT_DIFFERENCE.append(percent_diff)
+                        self.DAYS_DIFFERENCE.append(delta_between_drop_and_rise)
+                        self.DRAW_UP_DATE.append(draw_up_date)
+                        break;
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
