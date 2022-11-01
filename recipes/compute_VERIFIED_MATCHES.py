24f8a4e7b5c61d0bcb1cacb07cbd89685f3f1aa9
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-11-01 01:12:45 +0000

    Recipe NAFCUSTOMER.compute_VERIFIED_MATCHES saved through notebook notebook editor for compute_VERIFIED_MATCHES

--- recipes/compute_VERIFIED_MATCHES.py
+++ recipes/compute_VERIFIED_MATCHES.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -6,17 +7,23 @@ from dataiku import pandasutils as pdu
 # Read recipe inputs
 MATCHES_1_TO_1_QUEUED = dataiku.Dataset("MATCHES_1_TO_1_QUEUED")
 MATCHES_1_TO_1_QUEUED_df = MATCHES_1_TO_1_QUEUED.get_dataframe()
+
 MATCHES_1_TO_N_QUEUED = dataiku.Dataset("MATCHES_1_TO_N_QUEUED")
 MATCHES_1_TO_N_QUEUED_df = MATCHES_1_TO_N_QUEUED.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df_1_N = MATCHES_1_TO_1_QUEUED_df
+print(len(df_1_N))
+df_1_N.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-VERIFIED_MATCHES_df = ... # Compute a Pandas dataframe to write into VERIFIED_MATCHES
+#VERIFIED_MATCHES_df = ... # Compute a Pandas dataframe to write into VERIFIED_MATCHES
 
 
 # Write recipe outputs
-VERIFIED_MATCHES = dataiku.Dataset("VERIFIED_MATCHES")
-VERIFIED_MATCHES.write_with_schema(VERIFIED_MATCHES_df)
+#VERIFIED_MATCHES = dataiku.Dataset("VERIFIED_MATCHES")
+#VERIFIED_MATCHES.write_with_schema(VERIFIED_MATCHES_df)
