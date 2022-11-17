060970f9bbbce3bb9e9bb8f3bc937dae37734b23
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2022-11-17 11:12:29 +0000

    Recipe NAFCUSTOMER.compute_ACCOUNT_NEW_SALES_FULL saved through notebook notebook editor for compute_ACCOUNT_NEW_SALES_FULL

--- recipes/compute_ACCOUNT_NEW_SALES_FULL.py
+++ recipes/compute_ACCOUNT_NEW_SALES_FULL.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -6,16 +7,31 @@ from dataiku import pandasutils as pdu
 # Read recipe inputs
 WEX_and_Non_Managed_Sold_20190101_20220920 = dataiku.Dataset("WEX_and_Non_Managed_Sold_20190101_20220920")
 WEX_and_Non_Managed_Sold_20190101_20220920_df = WEX_and_Non_Managed_Sold_20190101_20220920.get_dataframe()
+
 Managed_Sold_20190101_20220920 = dataiku.Dataset("Managed_Sold_20190101_20220920")
 Managed_Sold_20190101_20220920_df = Managed_Sold_20190101_20220920.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+WEX_and_Non_Managed_Sold_20190101_20220920_df['DATA_SOURCE'] = 'WEX_and_Non_Managed_Sold'
+Managed_Sold_20190101_20220920_df['DATA_SOURCE'] = 'Managed_Sold'
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(len(WEX_and_Non_Managed_Sold_20190101_20220920_df))
+print(len(Managed_Sold_20190101_20220920_df))
+df = pd.concat([WEX_and_Non_Managed_Sold_20190101_20220920_df,Managed_Sold_20190101_20220920_df])
+print(len(df))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(len(df))
+df.drop_duplicates(subset=['SOURCE_ACCOUNT_ID'], inplace=True)
+print(len(df))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-ACCOUNT_NEW_SALES_FULL_df = ... # Compute a Pandas dataframe to write into ACCOUNT_NEW_SALES_FULL
-
+ACCOUNT_NEW_SALES_FULL_df = df
 
 # Write recipe outputs
 ACCOUNT_NEW_SALES_FULL = dataiku.Dataset("ACCOUNT_NEW_SALES_FULL")
