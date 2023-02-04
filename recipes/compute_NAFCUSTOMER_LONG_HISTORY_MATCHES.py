ea131892d52992e0d4a07391516f000ae41fbad9
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2023-02-04 15:54:34 +0000

    Recipe NAFCUSTOMER.compute_NAFCUSTOMER_LONG_HISTORY_MATCHES saved through notebook notebook editor for compute_NAFCUSTOMER_LONG_HISTORY_MATCHES

--- recipes/compute_NAFCUSTOMER_LONG_HISTORY_MATCHES.py
+++ recipes/compute_NAFCUSTOMER_LONG_HISTORY_MATCHES.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -7,15 +8,18 @@ from dataiku import pandasutils as pdu
 NAFCUSTOMER_ACCOUNT_ACTIVE_CARDS_BY_QUARTER = dataiku.Dataset("NAFCUSTOMER_ACCOUNT_ACTIVE_CARDS_BY_QUARTER")
 NAFCUSTOMER_ACCOUNT_ACTIVE_CARDS_BY_QUARTER_df = NAFCUSTOMER_ACCOUNT_ACTIVE_CARDS_BY_QUARTER.get_dataframe()
 
+NAFCUSTOMER_RDW_CONVERSIONS = dataiku.Dataset("NAFCUSTOMER_RDW_CONVERSIONS")
+NAFCUSTOMER_RDW_CONVERSIONS_df = NAFCUSTOMER_RDW_CONVERSIONS.get_dataframe()
 
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-NAFCUSTOMER_LONG_HISTORY_MATCHES_df = NAFCUSTOMER_ACCOUNT_ACTIVE_CARDS_BY_QUARTER_df # For this sample code, simply copy input to output
-NAFCUSTOMER_RDW_CONVERSIONS_df = NAFCUSTOMER_RDW_CONVERSIONS_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(len(NAFCUSTOMER_LONG_HISTORY_MATCHES_df))
+print(len(NAFCUSTOMER_RDW_CONVERSIONS_df))
 
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 
 # Write recipe outputs
 NAFCUSTOMER_LONG_HISTORY_MATCHES = dataiku.Dataset("NAFCUSTOMER_LONG_HISTORY_MATCHES")
