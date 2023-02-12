6d8aa94c14c8573582ce03162a5c89d0238e674b
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2023-02-12 22:27:22 +0000

    Recipe NAFCUSTOMER.compute_TENURE_CALC saved through notebook notebook editor for compute_TENURE_CALC

--- recipes/compute_TENURE_CALC.py
+++ recipes/compute_TENURE_CALC.py
@@ -1,20 +1,39 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+from datetime import date, datetime, timedelta
+import time
+
+t0 = time.time()
+
 # Read recipe inputs
 NAFCUSTOMER_REVENUE_AGGREGATED = dataiku.Dataset("NAFCUSTOMER_REVENUE_AGGREGATED")
 NAFCUSTOMER_REVENUE_AGGREGATED_df = NAFCUSTOMER_REVENUE_AGGREGATED.get_dataframe()
 
+t1 = time.time()
+print("load duration", (t1-t0)/60.0, "minutes")
 
+print(len(NAFCUSTOMER_REVENUE_AGGREGATED_df))
+NAFCUSTOMER_REVENUE_AGGREGATED_df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(len(NAFCUSTOMER_REVENUE_AGGREGATED_df))
+df = NAFCUSTOMER_REVENUE_AGGREGATED_df
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-TENURE_CALC_df = NAFCUSTOMER_REVENUE_AGGREGATED_df # For this sample code, simply copy input to output
+#TENURE_CALC_df = NAFCUSTOMER_REVENUE_AGGREGATED_df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
-TENURE_CALC = dataiku.Dataset("TENURE_CALC")
-TENURE_CALC.write_with_schema(TENURE_CALC_df)
+#TENURE_CALC = dataiku.Dataset("TENURE_CALC")
+#TENURE_CALC.write_with_schema(TENURE_CALC_df)
