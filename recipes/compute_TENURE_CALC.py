d5f7e2481a8254531a47125b8628c39a9b487539
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2023-02-13 20:44:19 +0000

    Recipe NAFCUSTOMER.compute_TENURE_CALC saved through notebook notebook editor for compute_TENURE_CALC

--- recipes/compute_TENURE_CALC.py
+++ recipes/compute_TENURE_CALC.py
@@ -4,6 +4,32 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+import matplotlib.pyplot as plt
+
+# Input View
+#
+#  CREATE VIEW DATAIKU_ANALYTICS_SUB.DATA_SCIENCE.NAFCUSTOMER_REVENUE_AGGREGATED
+#  AS
+#  SELECT
+#  CUSTOMER_ID,
+#  CUSTOMER,
+#  YEAR(REVENUE_DATE) as REVENUE_YEAR,
+#  MONTH(REVENUE_DATE) as REVENUE_MONTH,
+#  MIN(SETUP_DATE) AS SETUP_DATE,
+#  MIN(BI_CUS_PAR) as BI_CUS_PAR,
+#  MIN(BI_PRODUCT) as BI_PRODUCT,
+#  MIN(BI_MRU) as BI_MRU,
+#  SUM(REVENUE_AMOUNT_USD) as REVENUE_AMOUNT_USD,
+#  SUM(GROSS_SPEND_AMOUNT) as GROSS_SPEND_AMOUNT,
+#  SUM(PURCHASE_GALLONS_QTY) PURCHASE_GALLONS_QTY,
+#  SUM(TOTAL_ACTIVE_CARD_COUNT) as TOTAL_ACTIVE_CARD_COUNT
+#  FROM DATAIKU_ANALYTICS_SUB.DATA_SCIENCE.NAFCUSTOMER_REVENUE
+#  WHERE (FUNDED_BY IS NULL OR FUNDED_BY='Unknown' OR FUNDED_BY='WEX Funded')
+#  AND (NOT((REVENUE_CODE='26') AND (CUSTOMER_ID IS NULL)))
+#  AND (NOT((REVENUE_CODE='E8') AND (CUSTOMER_ID IS NULL)))
+#  GROUP BY 1,2,3,4
+
+
 from datetime import date, datetime, timedelta
 import time
 
@@ -20,18 +46,51 @@ print(len(NAFCUSTOMER_REVENUE_AGGREGATED_df))
 NAFCUSTOMER_REVENUE_AGGREGATED_df.head()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# create date column, combining month and year
-# group revenue sum by date column
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 print(len(NAFCUSTOMER_REVENUE_AGGREGATED_df))
 df = NAFCUSTOMER_REVENUE_AGGREGATED_df
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# cast year as integer, create a revenue date column at the monthly grain
+df['REVENUE_YEAR'] = df['REVENUE_YEAR'].astype('Int64')
+df['REVENUE_DATE'] = df['REVENUE_MONTH'].astype(str) + '-' + df['REVENUE_YEAR'].astype(str)
+df['REVENUE_DATE'] = pd.to_datetime(df['REVENUE_DATE'], format='%m-%Y').dt.strftime('%m-%Y')
+
+print(len(df))
+df = df[df.REVENUE_YEAR!=2023]
+print(len(df))
 df.head()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-df['REVENUE_DATE']
+import matplotlib.dates as mdates
+
+# aggregate revenue at annual grain
+df_revenue_per_year = df.groupby(['REVENUE_YEAR']).REVENUE_AMOUNT_USD.sum().reset_index()
+
+# plot out 
+fig, ax1 = plt.subplots(figsize=(16,5))
+ax1.plot(df_revenue_per_year['REVENUE_YEAR'],df_revenue_per_year['REVENUE_AMOUNT_USD'], marker='o')
+ax1.set_xlabel('YEAR', fontsize=14)
+ax1.set_ylabel('REVENUE', fontsize=14)
+ax1.grid()
+ax1.set_ylim(ymin=0)
+fig.autofmt_xdate()
+#ax1.xaxis.set_major_locator(mdates.YearLocator())
+plt.show()
+
+df_revenue_per_year.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(len(df))
+df['SETUP_DATE_DT'] = pd.to_datetime(df['SETUP_DATE'])
+df = df[df.SETUP_DATE_DT.dt.year<2019]
+print(len(df))
+df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+#print(len(df))
+#df = df[df.SETUP_DATE.dt.year<=2019]
+#print(len(df))
+df.head()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
