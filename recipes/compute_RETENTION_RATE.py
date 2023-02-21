4dbb32889a7d2b71cef96a1b4915a489afdaaace
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2023-02-21 19:13:19 +0000

    Recipe NAFCUSTOMER.compute_RETENTION_RATE saved through notebook notebook editor for compute_RETENTION_RATE

--- recipes/compute_RETENTION_RATE.py
+++ recipes/compute_RETENTION_RATE.py
@@ -1,22 +1,92 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+from datetime import date, datetime, timedelta
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import time
+
+t0 = time.time()
+print("loading...")
+
 # Read recipe inputs
 NAFCUSTOMER_ACCOUNT_ANNUAL_ACTIVE_CARD_COUNT = dataiku.Dataset("NAFCUSTOMER_ACCOUNT_ANNUAL_ACTIVE_CARD_COUNT")
 NAFCUSTOMER_ACCOUNT_ANNUAL_ACTIVE_CARD_COUNT_df = NAFCUSTOMER_ACCOUNT_ANNUAL_ACTIVE_CARD_COUNT.get_dataframe()
+
 NAFCUSTOMER_REVENUE_AGGREGATED = dataiku.Dataset("NAFCUSTOMER_REVENUE_AGGREGATED")
 NAFCUSTOMER_REVENUE_AGGREGATED_df = NAFCUSTOMER_REVENUE_AGGREGATED.get_dataframe()
 
+t1 = time.time()
+print("load duration", (t1-t0)/60.0, "minutes")
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# copy input datasets
+# this allows us to reset the analysis without having to reload the datasets from the database
+print(len(NAFCUSTOMER_ACCOUNT_ANNUAL_ACTIVE_CARD_COUNT_df), "rows in NAFCUSTOMER_ACCOUNT_ANNUAL_ACTIVE_CARD_COUNT")
+df_a = NAFCUSTOMER_ACCOUNT_ANNUAL_ACTIVE_CARD_COUNT_df.copy()
+
+print(len(NAFCUSTOMER_REVENUE_AGGREGATED_df), "rows in NAFCUSTOMER_REVENUE_AGGREGATED")
+df = NAFCUSTOMER_REVENUE_AGGREGATED_df.copy()
+# remove 2023 from the analysis as this is a partial year
+df = df[df.REVENUE_YEAR!=2023]
+print(len(df))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+
+# Group sum of revenue by customer and year 
+df_revenue_by_customer_and_year = df.groupby(['CUSTOMER_ID','CUSTOMER','REVENUE_YEAR']).REVENUE_AMOUNT_USD.sum().reset_index()
+print(len(df_revenue_by_customer_and_year))
+
+# Group sum of spend by customer and year
+df_spend_by_customer_and_year = df.groupby(['CUSTOMER_ID','CUSTOMER','REVENUE_YEAR']).GROSS_SPEND_AMOUNT.sum().reset_index()
+print(len(df_spend_by_customer_and_year))
+
+# Group active card count by customer and year
+df_active_card_count_by_customer = df_a.groupby(['CUSTOMER_ID','CUSTOMER','YEAR_NUMBER']).ACTIVE_CARD_COUNT.sum().reset_index()
+df_active_card_count_by_customer.columns = ['CUSTOMER_ID','CUSTOMER','REVENUE_YEAR','ACTIVE_CARD_COUNT']
+print(len(df_active_card_count_by_customer))
+
+# Assign max active card count year by customer
+# This is the fleet size of the customer
+df_customer_fleet_size = df_active_card_count_by_customer.groupby(['CUSTOMER_ID','CUSTOMER']).ACTIVE_CARD_COUNT.max().reset_index()
+df_customer_fleet_size.columns = ['CUSTOMER_ID', 'CUSTOMER', 'FLEET_SIZE']
+print(len(df_customer_fleet_size))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df_revenue_by_customer_and_year[df_revenue_by_customer_and_year.REVENUE_YEAR==2022].CUSTOMER_ID.nunique()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Join datasets together in order to create combined metrics at customer and year grain
+print(len(df_revenue_by_customer_and_year))
+df_j = pd.merge(df_revenue_by_customer_and_year,df_spend_by_customer_and_year,how='left',on=['CUSTOMER_ID','CUSTOMER','REVENUE_YEAR'])
+print(len(df_j))
+
+df_j = pd.merge(df_j,df_active_card_count_by_customer,how='left',on=['CUSTOMER_ID','CUSTOMER','REVENUE_YEAR'])
+print(len(df_j))
+
+df_j = pd.merge(df_j,df_customer_fleet_size, how='left',on=['CUSTOMER_ID','CUSTOMER'])
+print(len(df_j))
+
+df_j.loc[df_j.ACTIVE_CARD_COUNT.isnull(),'ACTIVE_CARD_COUNT'] = 0
+df_j.loc[df_j.FLEET_SIZE.isnull(),'FLEET_SIZE'] = 0
+
+df_j['FLEET_CATEGORY'] = 'NOT SET'
+df_j.loc[df_j.FLEET_SIZE.between(0,5),'FLEET_CATEGORY'] = '(<=5 cards)'
+df_j.loc[df_j.FLEET_SIZE.between(6,100),'FLEET_CATEGORY'] = '(between 6 and 100 cards)'
+df_j.loc[df_j.FLEET_SIZE>50,'FLEET_CATEGORY'] = '(>100 cards)'
+df_j.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-RETENTION_RATE_df = ... # Compute a Pandas dataframe to write into RETENTION_RATE
+#RETENTION_RATE_df = ... # Compute a Pandas dataframe to write into RETENTION_RATE
 
 
 # Write recipe outputs
-RETENTION_RATE = dataiku.Dataset("RETENTION_RATE")
-RETENTION_RATE.write_with_schema(RETENTION_RATE_df)
+#RETENTION_RATE = dataiku.Dataset("RETENTION_RATE")
+#RETENTION_RATE.write_with_schema(RETENTION_RATE_df)
