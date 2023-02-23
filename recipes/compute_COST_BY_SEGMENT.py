c76655beee1ebde78326880916b96af891013729
Daniel.Vandermeer <Daniel.Vandermeer@wexinc.com>
2023-02-23 02:58:43 +0000

    Saved recipe: NAFCUSTOMER.compute_COST_BY_SEGMENT

---
+++ recipes/compute_COST_BY_SEGMENT.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2019 = dataiku.Dataset("NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2019")
+NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2019_df = NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2019.get_dataframe()
+
+NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2021 = dataiku.Dataset("NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2021")
+NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2021_df = NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2021.get_dataframe()
+
+NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2020 = dataiku.Dataset("NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2020")
+NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2020_df = NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2020.get_dataframe()
+
+NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2022 = dataiku.Dataset("NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2022")
+NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2022_df = NAFCUSTOMER_REVENUE_AGGREGATED_MRU_2022.get_dataframe()
+
+# Compute recipe outputs
+# TODO: Write here your actual code that computes the outputs
+# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+
+COST_BY_SEGMENT_df = ... # Compute a Pandas dataframe to write into COST_BY_SEGMENT
+
+
+# Write recipe outputs
+COST_BY_SEGMENT = dataiku.Dataset("COST_BY_SEGMENT")
+COST_BY_SEGMENT.write_with_schema(COST_BY_SEGMENT_df)
